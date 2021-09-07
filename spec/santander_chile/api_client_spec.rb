f9020e1e0f5237131c79b5ea3460593ebce829ae
Jerry Mendoza <jamendoza@uc.cl>
2021-09-06 23:36:52 -0300

    Change minitest to rspec

---
+++ spec/santander_chile/api_client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe SantanderChile::ApiClient do
+  it "has a version number" do
+    expect(SantanderChile::ApiClient::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
