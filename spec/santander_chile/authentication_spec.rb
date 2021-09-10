30f66825df62025fd47f9a4ee82452114027f7d7
Jerry Mendoza <jamendoza@uc.cl>
2021-09-10 00:52:09 -0300

    Add env for testing

--- spec/santander_chile/authentication_spec.rb
+++ spec/santander_chile/authentication_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 RSpec.describe "Authentication" do
   let(:client) { config_client }
-  let(:username) { "00111111111" }
-  let(:password) { "1111" }
+  let(:username) { ENV["USERNAME"] }
+  let(:password) { ENV["PASSWORD"] }
 
   context "check login" do
     it "supports the login with username/password" do
@@ -11,7 +11,7 @@ RSpec.describe "Authentication" do
         username: username,
         password: password,
       )
-      expect(token).to raise be_instance_of SantanderChile::ApiClient::Token
+      expect(token).to be_instance_of SantanderChile::ApiClient::Token
     end
   end
 end
