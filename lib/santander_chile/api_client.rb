a2cdadc61e0e07e59827e8c9401b39276aea00fa
Jerry Mendoza <jamendoza@uc.cl>
2021-10-31 01:10:13 -0300

    Refactor autoload files

--- lib/santander_chile/api_client.rb
+++ lib/santander_chile/api_client.rb
@@ -1,47 +1,27 @@
 require "faraday"
 require "faraday_middleware"
 require "santander_chile/api_client/version"
-require "santander_chile/api_client/endpoints"
 
 module SantanderChile
   module ApiClient
     autoload :Error, "santander_chile/api_client/error"
     autoload :Token, "santander_chile/api_client/token"
 
+    #models
     autoload :Object, "santander_chile/api_client/models/object"
     autoload :Account, "santander_chile/api_client/models/account"
     autoload :Movement, "santander_chile/api_client/models/movement"
     autoload :Collection, "santander_chile/api_client/collection"
 
+    #resources
     autoload :Resource, "santander_chile/api_client/resources/resource"
     autoload :ProductsResource, "santander_chile/api_client/resources/products"
     autoload :MovementsResource, "santander_chile/api_client/resources/movements"
 
-    class Client
-      autoload :Configuration, "santander_chile/api_client/client/configuration"
-      autoload :Connection, "santander_chile/api_client/client/connection"
-      autoload :Authentication, "santander_chile/api_client/client/authentication"
-
-      include Authentication
-      attr_accessor :config
-
-      def initialize
-        self.config = Configuration.new
-        yield(config) if block_given?
-        config.validate!
-      end
-
-      def connection(host: BASE_URL)
-        Connection.new(client: self, host: host)
-      end
-
-      def products
-        ProductsResource.new(self).list
-      end
-
-      def movements(account)
-        MovementsResource.new(self).list(account)
-      end
-    end
+    #client
+    autoload :Configuration, "santander_chile/api_client/client/configuration"
+    autoload :Connection, "santander_chile/api_client/client/connection"
+    autoload :Authentication, "santander_chile/api_client/client/authentication"
+    autoload :Client, "santander_chile/api_client/client"
   end
 end
---
+++ lib/santander_chile/api_client/client.rb
@@ -0,0 +1,29 @@
+module SantanderChile
+  module ApiClient
+    class Client
+      AUTH_BASE_URL = "https://apideveloper.santander.cl/sancl/privado/Cliente/v1/"
+      BASE_URL = "https://apiper.santander.cl/appper/facade/"
+
+      include Authentication
+      attr_accessor :config
+
+      def initialize
+        self.config = Configuration.new
+        yield(config) if block_given?
+        config.validate!
+      end
+
+      def connection(host: BASE_URL)
+        Connection.new(client: self, host: host)
+      end
+
+      def products
+        ProductsResource.new(self).list
+      end
+
+      def movements(account)
+        MovementsResource.new(self).list(account)
+      end
+    end
+  end
+end
